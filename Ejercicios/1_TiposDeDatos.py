726b60f3990af12fdfb68cbfd450055c42bc2ad9
MichaelStiven12 <mivalenciam@unal.edu.co>
2022-10-06 15:44:50 -0500

    first commit

---
+++ Ejercicios/1_TiposDeDatos.py
@@ -0,0 +1,63 @@
+# Crear variables y asignarles los siguientes tipos de datos:
+# Enteros: 1, 2, 3, 999
+# Luego reste sucesivamente del ultimo al primero y almacenelo en una variable llamada resultado.
+
+a1=1
+b1=2
+c1=3
+d1=999
+
+resultado=d1-c1-b1-a1
+
+print("Resultado1 = ", resultado)
+
+# Flotantes: 15.2, 29.5, 18.28
+# Luego divida sucesivamente del primero al ultimo y almacenelo en una variable llamada resultado2.
+
+a2=15.2
+b2=29.5
+c2=18.28
+
+e2=a2/b2
+resultado2=e2/c2
+
+print("Resultado2 = ", resultado2)
+
+# Strings: "123", "Michael"
+# Luego sume ambas variables y determine si la operación es posible, si asi es almacenelo en una variable de su elección.
+
+a3="123 "
+b3="Michael"
+
+c3=a3+b3
+
+print(c3)
+
+########## Para pensar ############
+
+# Busque una manera de convertir :
+# Entero a flotante
+
+a4=5
+
+print(float(a4))
+
+# Flotante a entero
+
+b4=6.0
+
+print(int(b4))
+
+# String a entero y flotante
+
+c4="10"
+
+print(int(c4))
+print(float(c4))
+
+# Número a string
+
+d4=15
+
+print(str(d4))
+
---
+++ Ejercicios/8_Metodos.py
@@ -0,0 +1,30 @@
+cadena = "holamundocruel"
+
+#Extrae el primer elemento
+print(cadena[0])
+print(cadena[-14])
+
+#Extrae el ultimo elemento
+print(cadena[13])
+print(cadena[-1])
+
+#Extrae elementos de la mitad
+print(cadena[6]+cadena[7])
+
+
+#Metodos de las listas 
+
+#Metodos inplace, no se imprime pero si realiza un cambio en la lista
+
+lista = [1, "a", 2, 3, "b"]
+
+print(lista.append("1000"))
+print(lista.append(10))
+
+print(lista.pop(1))
+print(lista.pop(4))
+
+
+
+
+
